c7d2a931a460172c3c625fcdd0d173167f3d7190
Roberto Evangelista <betoesf@gmail.com>
2018-03-22 20:23:29 +0000

    Add edit Category

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -10,21 +12,28 @@ class Backoffice::CategoriesController < BackofficeController
   def create
     @category = Category.new(params_category)
     if @category.save
-      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi cadastrada com sucesso."
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi cadastrada com sucesso!"
     else
       render :new
     end
   end
 
   def edit
-
   end
 
   def update
-
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi atualizada com sucesso!"
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def params_category
     params.require(:category).permit(:description)
